2d97226cf797f082d40e6077e98089de95b73d65
Kenneth Tapang <kennethgabtapang@gmail.com>
2023-05-09 18:42:11 +0800

    Add project files.

---
+++ Midterm_1st_1.2/Program.cs
@@ -0,0 +1,30 @@
+﻿//Part 2
+
+namespace Midterm_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+
+            StreamReader sr = new StreamReader ("C:\\Users\\KENNETH GABRIEL\\source\\repos\\Midterm.txt");
+
+            line = sr.ReadToEnd();
+
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                line = sr.ReadLine();
+            }
+
+            sr.Close();
+
+            StreamWriter sw = new StreamWriter("C:\\Users\\KENNETH GABRIEL\\source\\repos\\Midterm.txt", true);
+
+            sw.WriteLine("Application Run Time " + DateTime.Now);
+            
+            sw.Close();
+        }
+    }
+}
